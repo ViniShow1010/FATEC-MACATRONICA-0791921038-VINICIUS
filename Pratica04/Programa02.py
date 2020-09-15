8d87484c7390d25500a130d2365984d63667c459
ViniShow1010 <62109307+ViniShow1010@users.noreply.github.com>
2020-09-15 08:31:54 -0300

    Create Programa02.py

---
+++ Pratica04/Programa02.py
@@ -0,0 +1,13 @@
+#Pede para o usario digitar uma senha e valida ela com a senha secreta
+#Cria uma variavel para uardar a senha
+senha_secreta = '123456'
+
+#Pede para o usuario digitar a senha senha_secreta
+senha = input('Informe sua senha: ')
+
+#Verifica se a senha esta correta
+if senha == senha_secreta:
+  print('Bem vindo Hackerman')
+
+if senha != senha_secreta:
+  print ('Acesso negado')
